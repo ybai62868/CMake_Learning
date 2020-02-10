d3f680967029e7c6ee1254d20fc03053c3688251
ybai62868 <ybai62868@gmail.com>
2020-02-10 22:14:22 +0800

    [Add] Hello World using CMake

---
+++ hello_world/main.c
@@ -0,0 +1,15 @@
+/*
+ * @Author: your name
+ * @Date: 2020-02-10 21:38:19
+ * @LastEditTime : 2020-02-10 22:13:42
+ * @LastEditors  : Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: /glow/Users/apple/Desktop/CMake_Learning/hello_world/main.c
+ */
+# include <stdio.h>
+
+int main(void)
+{
+    printf("Hello World use Cmake :) !\n");
+    return 0;
+}
